aac92227e359ca4ca87090c2e7f6fc117a361586
Sadaf Zamonova <milenkaya94@gmail.com>
2022-10-21 16:24:41 +0500

    feat: search input

--- components/search.tsx
+++ components/search.tsx
@@ -5,33 +5,23 @@ import { AddNamesProps, Cities } from '../types';
 
 export default function Search({ addNames, setNames, names }: AddNamesProps) {
   const [results, setResults] = useState<Cities[]>([]);
-
   const onChange = (e: any) => {
     const { value } = e.target;
     setNames(value);
-
     let matchingCities: any = [];
-
     if (value.length > 3) {
       for (let city of cities) {
         if (matchingCities.length >= 5) {
           break;
         }
-
         const match = city.name.toLowerCase().startsWith(value.toLowerCase());
-
         if (match) {
-
           matchingCities.push(city);
         }
       }
     }
-
     return setResults(matchingCities)
   };
-
-
-
 
   return (
     <div className='relative'>
@@ -46,7 +36,7 @@ export default function Search({ addNames, setNames, names }: AddNamesProps) {
         <ul   className='absolute pt-1 rounded-lg w-full m-0 border-2 border-solid border-inherit bg-white'>
           {results.length > 0 ? (
             results.map((city, index) => (
-              <li onClick={(e) => addNames(e)}  key={index}>
+              <li onClick={(e) => addNames(e)}  className="hover:cursor-pointer" key={index}>
                     {city.name}
                     {city.state ? `, ${city.state}` : ''}
                     <span >({city.country})</span>
